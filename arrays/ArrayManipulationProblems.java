abfdc075fe6af8b6555ba9acc1cc1a4a20cb25f1
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-05-10 19:20:25 +0530

    added array manipulation problems

---
+++ arrays/ArrayManipulationProblems.java
@@ -0,0 +1,56 @@
+package arrays;
+
+public class ArrayManipulationProblems {
+    // static void shiftBy1(int arr[]){
+    //     int n = arr.length;
+    //     int temp = arr[n-1];
+        
+    //     for(int i=n-1 ;  i > 0 ; i++){
+    //         arr[i] = arr[i - 1];
+    //     }
+    //     arr[0] = temp;
+    // }
+
+
+    // static void reverseArray(int arr[]){
+    //     int n = arr.length;
+    //     int i = 0;
+    //     int j = n-1;
+    //     while (i<=j){
+    //         int temp = arr[i];
+    //         arr[i] = arr[j];
+    //         arr[j] = temp;
+    //         i++;
+    //         j--;
+    //     }
+    // }
+    static void printAlternate(int arr[]){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        while(i<=j){
+            if(i==j){
+                System.out.print(arr[i] + " ");
+                i++;
+            } else{
+                System.out.print(arr[i] + " ");
+                i++;
+                System.out.print(arr[j] + " ");
+                j--;
+            }
+        }
+    
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        printAlternate(arr);
+        // reverseArray(arr);
+        // for(int num: arr){
+        //     System.out.println(num + " ");
+        // }
+        // shiftBy1(arr);
+        // for(int num: arr){
+        //     System.out.print(num + " ");
+        // }
+    }
+}
--- arrays/BasicProblems.java
+++ arrays/BasicProblems.java
@@ -34,38 +34,55 @@ public class BasicProblems {
         return false;
     }
 
-    static int[] getPosNegSum(int arr[]){
+    static int[] getPosNegSum(int arr[]) {
         int n = arr.length;
         int PosSum = 0;
         int NegSum = 0;
 
-        for(int i = 0; i < n; i++){
-            if(arr[i] > 0){
-                PosSum+= arr[i];
-            }
-            else{
-                NegSum+= arr[i];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                PosSum += arr[i];
+            } else {
+                NegSum += arr[i];
             }
         }
-        int ans[] = {PosSum,NegSum};
+        int ans[] = { PosSum, NegSum };
+        return ans;
+    }
+
+    static int[] getZeroOneCount(int arr[]) {
+        int n = arr.length;
+        int ZeroCount = 0;
+        int OneCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                ZeroCount++;
+            } else {
+                OneCount++;
+            }
+        }
+        int ans[] = { ZeroCount, OneCount };
         return ans;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 10, -20, 30, -40, 50 };
-        int ans[] = getPosNegSum(arr);
-        for (int value: ans){
-            System.out.println(value);
+        int[] arr = { 1, 1, 1, 0, 0, 0, 0 };
+        int ans[] = getZeroOneCount(arr);
+        System.out.println("Zero count: " + ans[0]);
+        System.out.println("One count: " + ans[1]);
+        // int ans[] = getPosNegSum(arr);
 
-        }
+        // System.out.println("Positive Sum = " + ans[0]);
+        // System.out.println("Negative Sum = " + ans[1]);
         // int result = AvgofArr(arr);
         // System.out.println("Average = " + result);
 
         // System.out.println(linearSearch(arr, 40));
-        
+
         // int[] result = MultiplyBy10(arr);
         // for (int value : result) {
-        //     System.out.print(value + " ");
+        // System.out.print(value + " ");
         // }
 
         // System.out.println();
