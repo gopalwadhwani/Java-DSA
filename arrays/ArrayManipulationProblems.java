2ec012d91498b8ea8fd90655855e2159c33aaa7a
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-05-23 12:19:54 +0530

    added more manipulation problems

--- arrays/ArrayManipulationProblems.java
+++ arrays/ArrayManipulationProblems.java
@@ -24,26 +24,38 @@ public class ArrayManipulationProblems {
     //         j--;
     //     }
     // }
-    static void printAlternate(int arr[]){
-        int n = arr.length;
-        int i = 0;
-        int j = n-1;
-        while(i<=j){
-            if(i==j){
-                System.out.print(arr[i] + " ");
-                i++;
-            } else{
-                System.out.print(arr[i] + " ");
-                i++;
-                System.out.print(arr[j] + " ");
-                j--;
-            }
-        }
+    // static void printAlternate(int arr[]){
+    //     int n = arr.length;
+    //     int i = 0;
+    //     int j = n-1;
+    //     while(i<=j){
+    //         if(i==j){
+    //             System.out.print(arr[i] + " ");
+    //             i++;
+    //         } else{
+    //             System.out.print(arr[i] + " ");
+    //             i++;
+    //             System.out.print(arr[j] + " ");
+    //             j--;
+    //         }
+    //     }
     
+    // }
+
+    static void PrintFirstSecondHalf(int arr[]){
+        int n = arr.length;
+        int mid = n/2;
+        for(int i=0; i<mid; i++){
+             System.out.print(arr[i] + " ");
+        }
+        for(int i=mid; i<n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        printAlternate(arr);
+        // printAlternate(arr);
         // reverseArray(arr);
         // for(int num: arr){
         //     System.out.println(num + " ");
